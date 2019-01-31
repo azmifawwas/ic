5ec2be2a69f46a6f28a11d8a9218f7a69a7c2402
azmifawwas <42112569+azmifawwas@users.noreply.github.com>
2019-01-31 19:27:31 +0700

    Update config.php

--- config.php
+++ config.php
@@ -3,13 +3,13 @@
 // user_name boleh kosong //
 $user_name="";
 //user_gaid wajib isi
-$user_gaid= "xxxxxxxxxxxxxxxxxx";
+$user_gaid= "8c611bc8-2fbf-47b5-8c1e-be1f3ad06984";
 //user_token wajib di isi //
-$user_token="xxxxxxxxxxxxxxxxxx";
+$user_token="AbyA0En7-3nLw5Vv";
 //user_id wajib di isi //
-$user_id="Facebook_xxxxxxxxxxxxxxxxxx";
+$user_id="Facebook_1474287339368064";
 // invite_code wajib di isi //
-$invite_code="xxxxxxxxxxxxxxxxxx";
+$invite_code="F6kdedDP9c";
 // Bearer wajib di isi //
-$Bearer="eyxxxxxxxxxxxxxxxxxx";
+$Bearer="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiJGYWNlYm9va18xNDc0Mjg3MzM5MzY4MDY0IiwiZXhwIjoiMTU1MTUyOTA1NDkxNyJ9.551wuLiN7BwBXbQrlF3IlvRH6lc5i1CuWoYEW8-Tzyk";
 ?>
